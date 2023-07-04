4cd1a64272303dd6d5eb89ab70d0fd1e9df03df3
adalbertobrant <adalbertobrant@gmail.com>
2023-07-04 04:50:47 -0300

    Criação do esqueleto inicial com a classe JFrame dos conversores

--- src/com/br/adalberto/conversormoedas/Menu.java
+++ src/com/br/adalberto/conversormoedas/Menu.java
@@ -1,5 +1,76 @@
 package com.br.adalberto.conversormoedas;
 
-public class Menu {
+import javax.swing.JFrame;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class Menu {
+	private JFrame frame;
+    private JComboBox<String> comboBox;
+    private String selectedOption;
+    
+    public Menu() {
+    	frame = new JFrame("Menu");
+        comboBox = new JComboBox<>(new String[]{"Conversor de Moedas"
+        		, "Conversor de Temperaturas"
+        		, "Conversor de Grandezas"});    
+    
+    // Configurando a ação do JComboBox
+	    comboBox.addActionListener(new ActionListener() {
+	        @Override
+	        public void actionPerformed(ActionEvent e) {
+	            selectedOption = (String) comboBox.getSelectedItem();
+	            switch(selectedOption) {
+	            case "Conversor de Grandezas":
+	            	openOtherScreen();
+	            	break;
+	            case "Conversor de Moedas":
+	            	openOtherScreen();
+	            	break;
+	            case "Conversor de Temperaturas":
+	            	openOtherScreen();
+	            	break;
+	            }
+	            /*if (selectedOption.equals("Conversor de Grandezas")) {
+	                // Navegar para a outra tela
+	                openOtherScreen();
+	            }*/
+	        }
+	    });
+	    frame.setLayout(new FlowLayout());
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    frame.setSize(800, 500);
+	    frame.add(comboBox);
+	    frame.setVisible(true);
+    }
+    
+    private void openOtherScreen() {
+        frame.dispose(); // Fecha a janela atual
+
+        JFrame otherFrame = new JFrame(this.selectedOption);
+        JButton backButton = new JButton("Voltar");
+
+        // Configurando a ação do botão de voltar
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                otherFrame.dispose(); // Fecha a janela atual
+                new Menu(); // Cria uma nova instância da classe Menu para voltar à tela anterior
+            }
+        });
+
+        otherFrame.setLayout(new FlowLayout());
+        otherFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        otherFrame.setSize(800, 500);
+        otherFrame.add(backButton);
+        otherFrame.setVisible(true);
+    }
+    
+    
+    
+    
+    
 }
