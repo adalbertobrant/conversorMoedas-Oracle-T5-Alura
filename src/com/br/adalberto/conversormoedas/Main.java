c5d273d520ac4885d8f29550c9fc0fb96982565b
adalbertobrant <adalbertobrant@gmail.com>
2023-07-04 04:52:26 -0300

    arquivo Main que chama a aplicação a partir do menu

---
+++ src/com/br/adalberto/conversormoedas/Main.java
@@ -0,0 +1,18 @@
+package com.br.adalberto.conversormoedas;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Menu();
+            }
+        });
+
+	}
+
+}
